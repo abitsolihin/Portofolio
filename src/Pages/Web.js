29ec01ec942c095d0b78970bec95c084021d00cb
Bitzz121 <abitsolihin8@gmail.com>
2022-08-06 02:06:32 +0700

    adding API

--- src/Pages/Web.js
+++ src/Pages/Web.js
@@ -1,43 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Style/ProjectPages.css';
 import { AiOutlineLink, AiOutlineGithub } from 'react-icons/ai';
+import axios from 'axios';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const WebsitePages = () => {
+  Aos.init();
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://projects-api121.herokuapp.com/projects')
+      .then((result) => {
+        setLoading(false);
+        setProjects(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        return <h1>Jaringan Ilang</h1>;
+      });
+  });
+
   return (
-    <div className="content-container">
-      <div className="content-wrapper">
-        <div className="content-title">
-          <h2>MoveZ Web Minting</h2>
+    <>
+      {loading ? (
+        <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <h1>Tunggu Bentar ya</h1>
         </div>
-        <div className="content-description">
-          <p>MoveZ Web Minting adalah sebuah Website Yang bertujuan untuk memperkenalkan sebuah project NFT dan melakukan Minting NFT didalamnya</p>
+      ) : (
+        <div className="content-container">
+          {projects.map((result, id) => {
+            return (
+              <div data-aos="fade-down" data-aos-delay="50" className="content-wrapper" key={id}>
+                <div data-aos="fade-down" data-aos-delay="100" className="content-title">
+                  <h2>{result.name}</h2>
+                </div>
+                <div data-aos="fade-down" data-aos-delay="150" className="content-description">
+                  <p>{result.description}</p>
+                </div>
+                <div className="content-links">
+                  <a data-aos="fade-down" data-aos-delay="200" href={result.linkweb}>
+                    <AiOutlineLink color="rgb(236, 240, 36)" size={20} />
+                  </a>
+                  <a data-aos="fade-down" data-aos-delay="250" href={result.linkgithub}>
+                    <AiOutlineGithub color="rgb(236, 240, 36)" size={20} />
+                  </a>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <div className="content-links">
-          <a href="https://movez.pages.dev/">
-            <AiOutlineLink color="rgb(236, 240, 36)" size={20} />
-          </a>
-          <a href="https://www.github.com/SolDuck/MoveZ">
-            <AiOutlineGithub color="rgb(236, 240, 36)" size={20} />
-          </a>
-        </div>
-      </div>
-      <div className="content-wrapper">
-        <div className="content-title">
-          <h2>Portofolio Class Website</h2>
-        </div>
-        <div className="content-description">
-          <p>Website Ini Berisi Biodata mengenai Siswa yang ada di satu kelas dan gallery kelas.</p>
-        </div>
-        <div className="content-links">
-          <a href="https://multimedia2.pages.dev/">
-            <AiOutlineLink color="rgb(236, 240, 36)" size={20} />
-          </a>
-          <a href="https://github.com/SolDuck/multimedia2.github.io">
-            <AiOutlineGithub color="rgb(236, 240, 36)" size={20} />
-          </a>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
